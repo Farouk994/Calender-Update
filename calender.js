75b3f3db61aa1115dc5f823d1cf104d53a27407d
Farouk <faroukkisuule55@gmail.com>
2020-12-12 18:28:58 -0500

    I then targetted my ids from the html file and created what changes that will be on my calender and created a table element with DOM to list down all dates and added DIVS for the squares within the calender

--- calender.js
+++ calender.js
@@ -43,6 +43,48 @@ var cal = {
           var blanks = endDay==0 ? 6 : 6-endDay;
           for (var i=0; i<blanks; i++) { squares.push("b"); }
         }
+        var container = document.getElementById("cal-container"),
+        cTable = document.createElement("table");
+    cTable.id = "calendar";
+    container.innerHTML = "";
+    container.appendChild(cTable);
+
     
+    var cRow = document.createElement("tr"),
+        cCell = null,
+        days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+    if (cal.sMon) { days.push(days.shift()); }
+    for (var d of days) {
+      cCell = document.createElement("td");
+      cCell.innerHTML = d;
+      cRow.appendChild(cCell);
     }
+    cRow.classList.add("head");
+    cTable.appendChild(cRow);
+
+  
+    var total = squares.length;
+    cRow = document.createElement("tr");
+    cRow.classList.add("day");
+    for (var i=0; i<total; i++) {
+      cCell = document.createElement("td");
+      if (squares[i]=="b") { cCell.classList.add("blank"); }
+      else {
+        cCell.innerHTML = "<div class='dd'>"+squares[i]+"</div>";
+        if (cal.data[squares[i]]) {
+          cCell.innerHTML += "<div class='evt'>" + cal.data[squares[i]] + "</div>";
+        }
+        cCell.addEventListener("click", function(){
+          cal.show(this);
+        });
+      }
+      cRow.appendChild(cCell);
+      if (i!=0 && (i+1)%7==0) {
+        cTable.appendChild(cRow);
+        cRow = document.createElement("tr");
+        cRow.classList.add("day");
+      }
+    }
+    cal.close();  
+    },
 }
